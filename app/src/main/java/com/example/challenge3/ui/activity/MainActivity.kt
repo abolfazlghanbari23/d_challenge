f3f1a86e7cc205a0f31be940bc4d3ee1c50c4d4a
abolf <abolfazlghanbarishce@gmail.com>
2021-12-23 10:13:39 +0330

    add packages and some initial changes

--- app/src/main/java/com/example/challenge3/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/challenge3/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.challenge3
+package com.example.challenge3.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.challenge3.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
